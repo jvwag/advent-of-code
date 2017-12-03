002203ac8b2f31e5672b789e87a6ccabdb2780d5
Joffrey van Wageningen <joffrey@munisense.com>
2017-12-03 10:22:01 +0100

    created assignment: 3/2017

---
+++ src/Year2017/Day3.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace jvwag\AdventOfCode\Year2017;
+
+use jvwag\AdventOfCode\Assignment;
+
+/**
+ * Class
+ *
+ * @package jvwag\AdventOfCode\Year2017
+ */
+class Day3 extends Assignment
+{
+    /**
+     * @return array
+     */
+    public function run(): array
+    {
+        // convert input
+        $input = (int) \trim($this->getInput());
+        $output2 = null;
+
+        // init vars
+        $c[0][0] = $s = 1;
+        $x = $y = $l = $i = $dir = 0;
+
+        // loop times given the input
+        while (++$i < $input) {
+            // change direction and step length
+            if (--$s === 0) {
+                $dir = ($dir + 1) % 4;
+                if ($dir % 2 === 1) {
+                    $l++;
+                }
+                $s = $l;
+            }
+
+            // update current coordinates
+            switch ($dir) {
+                case 0:
+                    $y--;
+                    break;
+                case 1:
+                    $x++;
+                    break;
+                case 2:
+                    $y++;
+                    break;
+                case 3;
+                    $x--;
+                    break;
+            }
+
+            // determine coordinate value using sum of surrounding values
+            $c[$x][$y] = 0;
+            foreach ([[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]] as [$dx, $dy]) {
+                $c[$x][$y] += $c[$x + $dx][$y + $dy] ?? 0;
+            }
+
+            // determine answer 2
+            if ($output2 === null && $c[$x][$y] > $input) {
+                $output2 = $c[$x][$y];
+            }
+        }
+
+        // determine answer 1
+        $output1 = abs($x) + abs($y);
+
+        // return answers
+        return
+            [
+                $output1,
+                $output2,
+            ];
+    }
+}
---
+++ tests/Year2017/Day3Test.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace jvwag\AdventOfCode\Tests\Year2017;
+
+use jvwag\AdventOfCode\Year2017\Day3;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class
+ *
+ * @package jvwag\AdventOfCode\Tests\Year2017
+ */
+class Day3Test extends TestCase
+{
+    /**
+     * @dataProvider part1DataProvider
+     *
+     * @param $input
+     * @param $expected
+     */
+    public function testDay3Part1($input, $expected)
+    {
+        $assignment = new Day3();
+        $assignment->setInput($input);
+        $output = $assignment->run();
+
+        self::assertSame($expected, $output[0]);
+    }
+
+    /**
+     * @return array
+     */
+    public function part1DataProvider(): array
+    {
+        return
+            [
+                ["1", 0],
+                ["12", 3],
+                ["23", 2],
+                ["1024", 31],
+            ];
+    }
+
+    /**
+     * @dataProvider part2DataProvider
+     *
+     * @param $input
+     * @param $expected
+     */
+    public function testDay3Part2($input, $expected)
+    {
+        $assignment = new Day3();
+        $assignment->setInput($input);
+        $output = $assignment->run();
+
+        self::assertSame($expected, $output[1], "Input: ".$input);
+    }
+
+    /**
+     * @return array
+     */
+    public function part2DataProvider(): array
+    {
+        return
+            [
+                ["6", 10],
+                ["7", 10],
+                ["8", 10],
+                ["9", 10],
+                ["10", 11],
+                ["11", 23],
+                ["22", 23],
+                ["23", 25],
+                ["24", 25],
+                ["25", 26],
+            ];
+    }
+}
