1d131881f7730c8f4874fbd7f2f0f9fe474f9cd2
Joffrey van Wageningen <joffrey@ne2000.nl>
2021-12-18 15:09:16 +0100

    Year2021\Day18

--- src/Year2021/Day18.php
+++ src/Year2021/Day18.php
@@ -18,60 +18,70 @@ class Day18 extends Assignment
     public function run(): array
     {
         // convert input
-        $input = explode("\n", trim($this->getInput()));
+        $numbers = explode("\n", trim($this->getInput()));
+        $count_of_numbers = count($numbers);
 
-        $sum = $this->add_and_reduce($input);
-        $output1 = $this->calculate_magnitude($sum);
+        // for the first part of the assignment we need add and reduce all the given numbers
+        $magnitude_of_reduction_of_all_numbers =
+            $this->calculate_magnitude(
+                $this->add_and_reduce($numbers)
+            );
 
-        $c = count($input);
+        // for part two of the assignment we need to find the biggest magnitude given two of the given numbers
+        $largest_magnitude_of_reduction_of_two_numbers = 0;
 
-        $max = 0;
-        for($a = 0; $a < $c; $a++) {
-            for($b = 0; $b < $c; $b++) {
-                if($a !== $b) {
-                    $sum = $this->add_and_reduce([$input[$a], $input[$b]]);
-                    $mag = $this->calculate_magnitude($sum);
-                    $max = max($mag, $max);
+        // so we loop over all possible combination of two numbers
+        for ($number1 = 0; $number1 < $count_of_numbers; $number1++) {
+            for ($number2 = 0; $number2 < $count_of_numbers; $number2++) {
+                // except if they are the same
+                if ($number1 !== $number2) {
+                    // get the maximum magnitude, when adding and reducing two of the numbers
+                    $largest_magnitude_of_reduction_of_two_numbers = max(
+                        $this->calculate_magnitude(
+                            $this->add_and_reduce([$numbers[$number1], $numbers[$number2]])
+                        ),
+                        $largest_magnitude_of_reduction_of_two_numbers
+                    );
                 }
             }
         }
 
-        // init output
-        $output2 = $max;
-
         // return answers
         return
             [
-                $output1,
-                $output2
+                $magnitude_of_reduction_of_all_numbers,
+                $largest_magnitude_of_reduction_of_two_numbers
             ];
     }
 
     public function calculate_magnitude(string $input): int
     {
-        while(preg_match("/\[(\d+),(\d+)\]/", $input, $match)) {
-            $input = preg_replace("/".preg_quote($match[0])."/", (string)((3 * $match[1]) + (2 * $match[2])), $input, 1);
+        // keep matching a pair "[x,y]" , from left to right
+        while (preg_match("/\[(\d+),(\d+)]/", $input, $match)) {
+            // replacing it with its magnitude calculation (3 * x + 3 * y)
+            $input = preg_replace("/" . preg_quote($match[0]) . "/", (string)((3 * $match[1]) + (2 * $match[2])), $input, 1);
         }
 
+        // the last value will have been replaced by a number, so convert it to an integer
         return (int)$input;
     }
 
     public function add_and_reduce(array $numbers): string
     {
-
         $output = null;
+
+        // loop over all given numbers
         foreach ($numbers as $number) {
+            // the first number will be the starting number
             if (!$output) {
                 $output = $number;
             } else {
-//                echo "  " . $output . PHP_EOL;
-//                echo "+ " . $number . PHP_EOL;
-                $output = $this->add($output, $number);
-                $output = $this->reduce($output);
-//                echo "= " . $output . PHP_EOL;
-            }
-//            echo PHP_EOL;
+                // following numbers will be added to each other
+                $output = "[" . $output . "," . $number . "]";
 
+                // after the addition, we reduce the value
+                $output = $this->reduce($output);
+            }
         }
 
         return $output;
@@ -79,83 +89,109 @@ class Day18 extends Assignment
 
     public function reduce(string $input): string
     {
-//        echo "  input: " . $input . PHP_EOL;
         $prev_input = null;
+        // loop over the input until it does not change anymore
         while ($prev_input !== $input) {
             $prev_input = $input;
 
+            // try to explode values in the input
             $input = $this->explode($input);
             if ($prev_input !== $input) {
-//                echo "explode: " . $input . PHP_EOL;
+                // if we did, we will loop over to see if we can explode again
                 continue;
             }
 
+            // and try to split the input
             $input = $this->split($input);
-            if ($prev_input !== $input) {
-//                echo "  split: " . $input . PHP_EOL;
-            }
         }
 
-
+        // return the input if we can not reduce it any further
         return $input;
-    }
-
-    public function add(string $a, string $b): string
-    {
-        return "[" . $a . "," . $b . "]";
     }
 
     public function explode($input)
     {
+        // init a depth value
         $depth = 0;
-        foreach (str_split($input) as $pos => $char) {
+        // loop over each character in the input
+        foreach (str_split($input) as $start_pos => $char) {
+            // and count the number of brackets to determine the depth
             if ($char === "[") {
                 $depth++;
             } elseif ($char === "]") {
                 $depth--;
             }
-            if ($depth > 4 && $input[$pos + 1] !== "[") {
-                if (preg_match("/^\[(\d+),(\d+)]/", substr($input, $pos))) {
-//                    echo "explode at depth: " . $depth . PHP_EOL;
-                    $start_pos = $pos;
-                    $end_pos = strpos($input, "]", $start_pos);
 
-//                    echo "explode: " . substr($input, $start_pos + 1, $end_pos - $start_pos - 1) . PHP_EOL;
-                    [$value_left, $value_right] = array_map("intval", (explode(",", substr($input, $start_pos + 1, $end_pos - $start_pos - 1))));
+            // if we find our self at a depth of more than four, and we match a pair of two integer numbers
+            if ($depth > 4 && preg_match("/^\[(\d+),(\d+)]/", substr($input, $start_pos), $match)) {
 
-                    $input = substr_replace($input, "0", $start_pos, $end_pos - $start_pos + 1);
+                // extract the values from the pair, and convert them to integers
+                [, $value_left, $value_right] = array_map("intval", $match);
 
-                    if (preg_match("/(\d+)([\[\],]+)$/", substr($input, 0, $start_pos), $match)) {
-                        $prev_value_len = strlen($match[1]);
-                        $prev_value_start_pos = $prev_value_len + strlen($match[2]);
-                        $prev_value = intval($match[1]);
-                        $new_value = (string)($prev_value + $value_left);
-                        $input = substr_replace($input, $new_value, $start_pos - $prev_value_start_pos, $prev_value_len);
-                        $start_pos += strlen($new_value) - $prev_value_len;
-                    }
+                // replace the pair of numbers with a '0' character
+                $input = substr_replace($input, "0", $start_pos, strlen($match[0]));
 
-                    if (preg_match("/^([\[\],]+)(\d+)/", substr($input, $start_pos + 1), $match)) {
-                        $next_value_len = strlen($match[2]);
-                        $next_value_start_pos = strlen($match[1]) + 1;
-                        $next_value = intval($match[2]);
-                        $input = substr_replace($input, (string)($next_value + $value_right), $start_pos + $next_value_start_pos, $next_value_len);
-                    }
-                    break;
-                } else {
-//                    echo "no explode at ".substr($input, $pos).PHP_EOL;
+                // find a number before the position in the string we have matched
+                if (preg_match("/(\d+)([\[\],]+)$/", substr($input, 0, $start_pos), $match)) {
+                    // replace that number we found with the sum of its original number and the left part of the pair
+                    $input = substr_replace(
+                        $input,
+                        // the new value we want to insert
+                        (string)(intval($match[1]) + $value_left),
+                        // the position were we need to replace is the starting position
+                        // minus the length of the value we matched,
+                        // minus the length of the characters between the starting position and the value
+                        $start_pos - (strlen($match[1]) + strlen($match[2])),
+                        // replace the length of characters we originally found to the left
+                        strlen($match[1])
+                    );
+
+                    // because we will use the starting position again to find a number to the right
+                    // we update the starting position with the difference in length between the originally found
+                    // value and the newly inserted value... the newly inserted value could have made the string
+                    // a bit longer...
+                    $start_pos += strlen((string)(intval($match[1]) + $value_left)) - strlen($match[1]);
                 }
+
+                // we do the same finding a number to the left of the found pair
+                if (preg_match("/^([\[\],]+)(\d+)/", substr($input, $start_pos + 1), $match)) {
+                    $input = substr_replace(
+                        $input,
+                        // the new value we will insert
+                        (string)(intval($match[2]) + $value_right),
+                        // the starting position is the number of characters between the pair and the first number, but
+                        // plus one because we will also count the inserted '0' character
+                        $start_pos + strlen($match[1]) + 1,
+                        // and the length of the matched original character
+                        strlen($match[2])
+                    );
+                }
+
+                // if we have done a replacement, we will break the loop
+                break;
             }
         }
+
+        // return the given, or updated input
         return $input;
     }
 
     public function split($input)
     {
-
+        // find the first occurrence of a number with double digits (same as > 9)
         if (preg_match("/(\d\d+)/", $input, $match)) {
-            $input = preg_replace("/(\d\d+)/", "[" . floor($match[1] / 2) . "," . ceil($match[1] / 2) . "]", $input, 1);
+            // replace these double digits (by finding them again
+            $input = preg_replace(
+                "/(\d\d+)/",
+                // with a pair, divided by two, with the remainder in the second value
+                "[" . floor($match[1] / 2) . "," . ceil($match[1] / 2) . "]",
+                // in the input
+                $input,
+                // but only one (the first)
+                1);
         }
 
+        // return the given, or updated input
         return $input;
     }
 }
--- tests/Year2021/Day18Test.php
+++ tests/Year2021/Day18Test.php
@@ -18,11 +18,22 @@ class Day18Test extends AssignmentTestCase
     public function testDay18(): void
     {
         $assignment = new Day18();
-        $assignment->setInput("");
+        $assignment->setInput(
+            "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]\n" .
+            "[[[5,[2,8]],4],[5,[[9,9],0]]]\n" .
+            "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]\n" .
+            "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]\n" .
+            "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]\n" .
+            "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]\n" .
+            "[[[[5,4],[7,7]],8],[[8,3],8]]\n" .
+            "[[9,3],[[9,9],[6,[4,9]]]]\n" .
+            "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]\n" .
+            "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]\n"
+        );
         $output = $assignment->run();
 
-        self::assertEquals(null, $output[0]);
-        self::assertEquals(null, $output[1]);
+        self::assertEquals(4140, $output[0]);
+        self::assertEquals(3993, $output[1]);
     }
 
     /**
